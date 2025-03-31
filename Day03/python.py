520bda1199b819daa2b7a8c28debc299f1341970
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2025-03-31 21:37:26 +0530

    Python

---
+++ Day03/python.py
@@ -0,0 +1,30 @@
+x = 5;
+#this is the to write if else in python...
+if x>3:
+    print("Yes It is greater than 3")
+else:
+    print("No It is not greater than 3")
+    
+#Iterative Statements
+
+#ye har baar ek character lega and print karwa dega.
+a = "Piyush"
+for i in a:
+    print(i)
+
+# ye utna baar print karwayega jitna character hai a mein.
+for i in a:
+    print("Hello");
+    
+# isme ye 0 se lekar 19 tak print karega. ek kaam print krta hai ye 
+for i in range(20):
+    print(i);
+    
+#ye start karega 2 se aur 4 tak chalega
+for i in range(2,5):
+    print(i);
+    
+#range function mien 3 value daal skte hai start, stop, step
+#isme ye start hoga 2 se jayega 10 se 1 kam tak and 3 plus hote rahega 
+for i in range(2,10,3):
+    print(i);
