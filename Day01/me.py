ab33d1d9904dafc7f52c2da258261f7d99e9bfd0
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2025-03-31 20:26:09 +0530

    Python

--- Day01/me.py
+++ Day01/me.py
@@ -1,4 +1,32 @@
 x=5
 print(x);
 
-print("Hello World");  
+print("Hello World");
+
+#this will five the type of variable.
+print(type(x));
+# python mein single quote mein bhi chalega 10 quote mein bhi chalega aur 100 quote mein 
+# bhi chalega 
+print('Hello World');
+
+
+#this is called complex variable in python...
+a = 14+2j;
+b = 14+5j;
+print(a+b);
+
+#list=square brackets
+m=[1,2,3,4,5,6,7.3,"hii",9];
+
+#tuple = round brackets or without brackets
+n=1,2,3,4,5,6,7,8,9;
+
+#set = curly brackets
+# set mein duplicate value nahi hoti hai
+o={1,2,3,4,5,6,7.2,81,"Hii"};
+
+print(m,n,o);
+
+
+''' ''' # this is called multi line comment
+# this is called single line comment
---
+++ Day02/me.py
@@ -0,0 +1,25 @@
+#this input function is used to get the input from the user
+x = input();
+print("The input is :-"+x)
+
+#another way to do this 
+y=input("Enter the value of y: ");
+
+#input fuction jab koi data leta hai na to wo string type ka data leta hai 
+# agar humko usko plus krke print karana hai to pehele usko humko 
+# typecast karna padega
+x=int(x) #like this 
+
+
+s = 10;
+print(id(s)) #this will give the address of the variable s;
+
+#python will not create new address for 10 it will refer d to the same address.
+d = 10;
+print(id(s))
+
+
+n=5
+v=5
+print(n==v); #ye values ko compare karega
+print(n is y) #ye address ko compare karega ki same hai ki nahi 
